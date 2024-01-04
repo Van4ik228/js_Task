8a12274428b21fde21cf891a4db8b769f14bd6d9
Van4ik228 <xzzz9556@gmail.com>
2024-01-04 18:45:30 +0000

    const /

--- Task4/tas4.js
+++ Task4/tas4.js
@@ -1,21 +1,21 @@
 // function adunare(a, b) {
 //     return a + b;
 //   }
-//   let total  = adunare(5, 2);
+//   const total  = adunare(5, 2);
 //   console.log(total);  
 // //--------------------------------------------------------------------------  
 //   function fahrenheitToCelsius(fahrenheit) {
-//     let celsius = (fahrenheit - 32) * 5/9;
+//     const celsius = (fahrenheit - 32) * 5/9;
 //     return celsius;
 //   }
-//   let temperaturaFahrenheit = 32;
-//   let temperaturaCelsius = fahrenheitToCelsius(temperaturaFahrenheit);
+//   const temperaturaFahrenheit = 32;
+//   const temperaturaCelsius = fahrenheitToCelsius(temperaturaFahrenheit);
 //   console.log(temperaturaFahrenheit + " градусов " + temperaturaCelsius.toFixed(2) + " градусов цельсия.");
 // //--------------------------------------------------------------------------  
 // function number(num) {
 //     return num % 2 === 0;
 //   }
-//   var num1= 10;
+//   const num1= 10;
 //   console.log("цифра " + num1 + " isPair? " + number(num1));
 //--------------------------------------------------------------------------
 // function factorial(n) {
@@ -25,48 +25,48 @@
 //       return n * factorial(n - 1);
 //     }
 //   }
-//   let num = 2;
+//   const num = 2;
 //   console.log("Факториал " + num + " равен: " + factorial(num));
 //--------------------------------------------------------------------------
 // function ArithmeticAverage(numbers) {
 //     if (numbers.length === 0) {
 //       return null; 
 //     }
-//     let sum = 0;
+//     const sum = 0;
 //     for (let i = 0; i < numbers.length; i++) {
 //       sum += numbers[i];
 //     }
   
-//     var average = sum / numbers.length;
+//     const average = sum / numbers.length;
 //     return average;
 //   }
-//   var num4 = [1, 2, 3, 4, 5];
-//   var middle = ArithmeticAverage(числа);
+//   const num4 = [1, 2, 3, 4, 5];
+//   const middle = ArithmeticAverage(числа);
 //   console.log("Среднее арифметическое чисел " + num4 + " равно: " + middle);
 //--------------------------------------------------------------------------
 // function reverseString(str) {
 //   return str.split('').reverse().join('');
 // }
-// let originalString = "Hello, World!";
-// let reversedString = reverseString(originalString);
+// const originalString = "Hello, World!";
+// const reversedString = reverseString(originalString);
 // console.log(reversedString);
 //--------------------------------------------------------------------------
 // function randomNumber(min, max) {
 //   return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
-// let result = randomNumber(10, 20);
+// const result = randomNumber(10, 20);
 // console.log(result);
 //--------------------------------------------------------------------------
 // function power(base, exponent) {
 //   return Math.pow(base, exponent);
 // }
-// let result = power(2, 3);
+// const result = power(2, 3);
 // console.log(result);
 //--------------------------------------------------------------------------
 function countVowels(str) {
   str = str.toLowerCase();
-  let vowels = "aeiou";
-  let count = 0;
+  const vowels = "aeiou";
+  const count = 0;
   for (var i = 0; i < str.length; i++) {
     if (vowels.indexOf(str[i]) !== -1) {
       count++;
@@ -74,6 +74,6 @@ function countVowels(str) {
   }
   return count;
 }
-var result = countVowels("Hello, World!");
+const result = countVowels("Hello, World!");
 console.log(result);
 //--------------------------------------------------------------------------
